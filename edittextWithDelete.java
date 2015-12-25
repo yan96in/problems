4b6230c33b1869346b2d4c3a65890cce81db9335
yan96in <yan96in@users.noreply.github.com>
2015-12-25 11:45:53 +0800

    Update edittextWithDelete.java

--- edittextWithDelete.java
+++ edittextWithDelete.java
@@ -1,4 +1,4 @@
-/*自定义EditView控件*/
+/*自定义EditView控件1*/
 public class EditTextWithDelete extends EditText {
     Context context;
     Drawable deleteAllOn;//点击抬起时候的删除图标
@@ -96,4 +96,76 @@ public class EditTextWithDelete extends EditText {
     protected void finalize() throws Throwable {
         super.finalize();
     }
+}
+
+//自定义控件2，两种都未测试，请自行测试
+public class EditTextWithDel extends EditText {
+	private final static String TAG = "EditTextWithDel";
+	private Drawable imgInable;
+	private Drawable imgAble;
+	private Context mContext;
+
+	public EditTextWithDel(Context context) {
+		super(context);
+		mContext = context;
+		init();
+	}
+
+	public EditTextWithDel(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		mContext = context;
+		init();
+	}
+
+	public EditTextWithDel(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mContext = context;
+		init();
+	}
+	
+	private void init() {
+		imgInable = mContext.getResources().getDrawable(R.drawable.delete_gray);
+		imgAble = mContext.getResources().getDrawable(R.drawable.delete);
+		addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {}
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+			@Override
+			public void afterTextChanged(Editable s) {
+				setDrawable();
+			}
+		});
+		setDrawable();
+	}
+	
+	//设置删除图片
+	private void setDrawable() {
+		if(length() < 1)
+			setCompoundDrawablesWithIntrinsicBounds(null, null, imgInable, null);
+		else
+			setCompoundDrawablesWithIntrinsicBounds(null, null, imgAble, null);
+	}
+	
+	 // 处理删除事件
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (imgAble != null && event.getAction() == MotionEvent.ACTION_UP) {
+            int eventX = (int) event.getRawX();
+            int eventY = (int) event.getRawY();
+            Log.e(TAG, "eventX = " + eventX + "; eventY = " + eventY);
+            Rect rect = new Rect();
+            getGlobalVisibleRect(rect);
+            rect.left = rect.right - 50;
+            if(rect.contains(eventX, eventY)) 
+            	setText("");
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
 }
